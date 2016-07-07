de0e47c313335bf2918d9ea456dfbcf1fe33586a
Cazacu Tudor <cazacu.tudor@agilefreaks.com>
2016-07-07 10:36:39 +0300

    Config setup.py

--- gripit-modbus-master/data_reader.py
+++ gripit-modbus-master/data_reader.py
@@ -12,16 +12,34 @@ import time
 FIRST_SLAVE_ADDRESS = 2
 LAST_SLAVE_ADDRESS = 2
 
+REGISTERS_FIRST_ADDRESS = 1;
+REGISTERS_LAST_ADDRESS = 4;
+
 client = None
 current_file_name = None
+kwargs = {
+	"method": "rtu",
+	"stopbits": 1,
+	"bytesize": 8,
+	"parity": "N",
+	"baudrate": 115200,
+	"port": "/dev/ttyAMA0"
+}
 
 current_milli_time = lambda: int(round(time.time() * 1000))
+
+def read_slave_registers(slave_id):
+	return client.read_input_registers(
+		address=REGISTERS_FIRST_ADDRESS,
+		count=REGISTERS_LAST_ADDRESS,
+		unit=slave_id
+	)
 
 def read_async(should_stop_callback):
 	global client
 	for slave_id in range(FIRST_SLAVE_ADDRESS, LAST_SLAVE_ADDRESS + 1):
 		try:
-			res = client.read_input_registers(address=1, count=4, unit=slave_id)
+			res = read_slave_registers(slave_id)
 		except ConnectionException as ex:
 			print("ConnectionException: %s" % str(ex))
 			sys.exit()
@@ -33,51 +51,40 @@ def read_async(should_stop_callback):
 def async_reply(result, slave_id, should_stop_callback):
 	global current_file_name
 	if isinstance(result, ReadInputRegistersResponse):
-		file.write(current_file_name, result.registers, slave_id)
+		logger.write(current_file_name, result.registers, slave_id)
 	else:
 		print("ERROR: %s" % str(result))
-	if (not should_stop_callback()):
-		read_async(should_stop_callback)
-	else:
+	if (should_stop_callback()):
 		tornado.ioloop.IOLoop.instance().stop()
+	else:
+		read_async(should_stop_callback)
 
 def start_async_loop(should_stop_callback):
 	global client
-	client = AsyncModbusSerialClient(
-		method='rtu',
-		stopbits=1,
-		bytesize=8,
-		parity='N',
-		baudrate=115200,
-		port='/dev/ttyAMA0'
-	)	
+	global kwargs
+
+	client = AsyncModbusSerialClient(**kwargs)	
 	read_async(should_stop_callback)
 	tornado.ioloop.IOLoop.instance().start()
-		
+
 def start_sync_loop(should_stop_callback):
 	global client	
 	global current_file_name
-	client = ModbusClient(
-		method='rtu',
-		stopbits=1,
-		bytesize=8,
-		parity='N',
-		baudrate=115200,
-		port='/dev/ttyAMA0'
-	)
+	global kwargs
+
+	client = ModbusClient(**kwargs)
 	client.connect()
 	should_stop = should_stop_callback()
 	while not should_stop:
 		for slave_id in range(FIRST_SLAVE_ADDRESS, LAST_SLAVE_ADDRESS + 1):
-			result = client.read_input_registers(address=1, count=8, unit=slave_id)
+			result = read_slave_registers(slave_id)
 			logger.write(current_file_name, result.registers, slave_id)
 			should_stop = should_stop_callback()
 			if not should_stop:
 				break
-		
+
 def start(should_stop_callback):
 	global current_file_name
 	current_file_name = str(current_milli_time()) + '.csv'
 	logger.create(current_file_name)
 	start_sync_loop(should_stop_callback)
-	
--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup
 
 setup(
-	name='GripitModbusMasterAsync',
-    version='1.0',
-    description='GripIt Master Async Version',
+	name='GripitModbusMaster',
+    version='1.0.0',
+    description='GripIt Master',
     url='http://github.com/agilefreaks',
     author='Agilefreaks',
     author_email='agilefreaks@agilefreaks.com',
     license='MIT',
-    packages=['gripit-modbus-master-async'],
+    packages=['gripit-modbus-master'],
 	install_requires=[
-		'python3'
+		'RPi.GPIO >= 0.6.2',
+		'pymodbus >= 1.2.0'
 	],
 	dependency_links=[
-		'http://github.com/Agilefreaks/pymodbus/tarball/python3'
+		'http://github.com/Agilefreaks/pymodbus/tarball/python3#egg=pymodbus-1.2.0'
 	],
     zip_safe=False
 )
